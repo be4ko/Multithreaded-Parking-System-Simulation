dfab9adcae6f1ca585fbd8c27ab748f289447db9
hossamabohadima <hossamabohadima@gmail.com>
2024-11-10 00:41:04 +0200

    parkingSlots

---
+++ src/ParkingSlots.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class ParkingSpots {
+    List<Car> slots;
+
+    public ParkingSpots() {
+        slots = new ArrayList<>(4);
+        for (int i = 0; i < 4; i++) {
+            slots.add(null);
+        }
+    }
+
+    public synchronized boolean addCar(Car car) {
+        for (int i = 0; i < slots.size(); i++) {
+            if (slots.get(i) == null) {
+                slots.set(i, car);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public synchronized void deleteCar(Car car) {
+        for (int i = 0; i < slots.size(); i++) {
+            if (slots.get(i) != null && slots.get(i).carNumber == car.carNumber) {
+                slots.set(i, null);
+                break;
+            }
+        }
+    }
+
+    public synchronized int getOccupiedSpots() {
+        int count = 0;
+        for (Car car : slots) {
+            if (car != null) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
---
+++ src/Semaphore.java
@@ -0,0 +1,25 @@
+class Semaphore {
+    protected int value = 0;
+
+    Semaphore(int value) {
+        this.value = value;
+    }
+
+    public synchronized void P() {
+        value--;
+        if (value < 0) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+
+    public synchronized void V() {
+        value++;
+        if (value <= 0) {
+            notify();
+        }
+    }
+}
