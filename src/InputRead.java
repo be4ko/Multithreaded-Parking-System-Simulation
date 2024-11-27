17e2f14a15eaa6f53d726f432f41da8ca659552d
Mohamed Kaseb <114490535+MKaseb1@users.noreply.github.com>
2024-11-28 01:22:09 +0200

    Updated InputRead.java

--- src/InputRead.java
+++ src/InputRead.java
@@ -1,34 +1,32 @@
 import java.io.*;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 public class InputRead {
 
-    public static Map<Integer, ArrayList<Car>> readInput(String filename, ParkingSpots parkingSpots, Semaphore semaphore) {
-        Map<Integer, ArrayList<Car>> gateCarsMap = new HashMap<>();
+    public static ArrayList<Car> readInput(String filename, ParkingSpots parkingSpots,
+            Semaphore semaphore, ArrayList<Gate> gates) {
+        ArrayList<Car> cars = new ArrayList<>();
 
         try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
             String line;
             while ((line = br.readLine()) != null) {
                 String[] parts = line.split(", ");
-                if (parts.length != 4) continue;
+                if (parts.length != 4)
+                    continue;
 
                 int gateId = Integer.parseInt(parts[0].split(" ")[1]);
                 int carId = Integer.parseInt(parts[1].split(" ")[1]);
                 int arriveTime = Integer.parseInt(parts[2].split(" ")[1]);
                 int parkTime = Integer.parseInt(parts[3].split(" ")[1]);
 
-                Gate gate = new Gate(gateId, gateCarsMap.getOrDefault(gateId, new ArrayList<>()));
-                Car car = new Car(carId, arriveTime, parkTime, gate, parkingSpots, semaphore);
+                Car car = new Car(carId, arriveTime, parkTime, gates.get(gateId - 1), parkingSpots, semaphore);
+                cars.add(car);
 
-                gateCarsMap.putIfAbsent(gateId, new ArrayList<>());
-                gateCarsMap.get(gateId).add(car);
             }
         } catch (IOException e) {
             System.err.println("Error reading file: " + e.getMessage());
         }
 
-        return gateCarsMap;
+        return cars;
     }
 }
