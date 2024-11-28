a66d432009292cafa03ce413f7c4e07ce290d501
Hossamabohadima <147110912+Hossamabohadima@users.noreply.github.com>
2024-11-28 20:04:54 +0200

    test

--- src/Car.java
+++ src/Car.java
@@ -23,11 +23,10 @@ public class Car extends Thread {
     @Override
     public void run() {
         try {
-            Thread.sleep(arrivalTime * 1000);
+            Thread.sleep(arrivalTime * 50);
             System.out.println(
                     "Car " + carNumber + " from Gate " + gate.getGateNumber() + " arrived at time " + arrivalTime);
 
-            // Wait for an available semaphore
             semaphore.P();
             boolean parked = false;
 
@@ -38,23 +37,23 @@ public class Car extends Thread {
                     System.out.println("Car " + carNumber + " from Gate " + gate.getGateNumber()
                             + " parked. (Parking Status: " + parkingSpots.getOccupiedSpots() + " spots occupied)");
                 }
-            }
-
-            // If parking failed (shouldn't happen due to semaphore)
+             // If parking failed (shouldn't happen due to semaphore)
             if (!parked) {
                 System.out
                         .println("Car " + carNumber + " from Gate " + gate.getGateNumber() + " waiting for a spot.");
             }
+            }
+
 
             // Simulate parking duration
-            Thread.sleep(parkingDuration * 1000);
+            Thread.sleep(parkingDuration * 50);
 
             // Leave parking
             synchronized (parkingSpots) {
                 parkingSpots.deleteCar(this);
                 System.out.println("Car " + carNumber + " from Gate " + gate.getGateNumber()
-                        + " left after " + parkingDuration + " units of time.");
-                System.out.println("(Parking Status: " + parkingSpots.getOccupiedSpots() + " spots occupied)");
+                        + " left after " + parkingDuration + " units of time."+"(Parking Status: " + parkingSpots.getOccupiedSpots() + " spots occupied)");
+
             }
 
             // Release semaphore
@@ -63,7 +62,6 @@ public class Car extends Thread {
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
             System.err.println("Car " + carNumber + " was interrupted.");
-            // Release semaphore in case of interruption
             semaphore.V();
         }
     }
